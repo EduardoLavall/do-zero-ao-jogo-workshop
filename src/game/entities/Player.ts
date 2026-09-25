ea71dad8af34cddc78d5bd4525e1455ed7d72cc6
Eduardo Dias <132724986+EduardoLavall@users.noreply.github.com>
2026-09-25 16:49:26 -0300

    feat: adapt player for fixed-room transitions

--- src/game/entities/Player.ts
+++ src/game/entities/Player.ts
@@ -2,10 +2,10 @@ import type Phaser from "phaser";
 
 import type { PlayerInputState } from "../types";
 
-const PLAYER_WIDTH = 44;
-const PLAYER_HEIGHT = 72;
-const MOVE_SPEED = 360;
-const JUMP_VELOCITY = -560;
+const PLAYER_WIDTH = 66;
+const PLAYER_HEIGHT = 108;
+const MOVE_SPEED = 520;
+const JUMP_VELOCITY = -760;
 
 export class Player {
   public readonly body: Phaser.Physics.Arcade.Body;
@@ -14,12 +14,12 @@ export class Player {
   public constructor(scene: Phaser.Scene, x: number, y: number) {
     this.view = scene.add
       .rectangle(x, y, PLAYER_WIDTH, PLAYER_HEIGHT, 0x8be9fd)
-      .setStrokeStyle(3, 0xf7f3e8);
+      .setStrokeStyle(4, 0xf7f3e8);
 
     scene.physics.add.existing(this.view);
 
     this.body = this.view.body as Phaser.Physics.Arcade.Body;
-    this.body.setCollideWorldBounds(true);
+    this.body.setCollideWorldBounds(false);
     this.body.setSize(PLAYER_WIDTH, PLAYER_HEIGHT);
   }
 
@@ -39,7 +39,20 @@ export class Player {
     this.view.setFillStyle(input.controlEnabled ? 0x8be9fd : 0x64748b);
   }
 
+  public setPosition(x: number, y: number): void {
+    this.view.setPosition(x, y);
+    this.body.reset(x, y);
+  }
+
+  public stopHorizontal(): void {
+    this.body.setVelocityX(0);
+  }
+
   public get x(): number {
     return this.view.x;
+  }
+
+  public get y(): number {
+    return this.view.y;
   }
 }
